9aaf72f5e130680b0a4783035eb5fde90ceb636d
mohit019830 <mohitvaghasiya15@gmail.com>
2026-09-09 16:52:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express=require('express')
+const path=require('path')
+const fs=require('fs')
+const crypto=require('crypto')
+
+const app=express()
+const filePath=path.join(__dirname,'data.json')
+app.use(express.json())
+
+
+//1 POST /api/items — Report an item
+app.post("/",(req,res)=>{
+    try{
+        const data=fs.readFileSync(filePath,"utf-8")
+        const data1=JSON.parse(data)
+        const {itemName,type,place,date,contact}=req.body
+        const id=crypto.randomUUID();
+        let obj={
+            "id":id,
+            "itemName":itemName,
+            "type":type,
+            "place":place,
+            "date":date,
+            "contact":contact,
+            "status":"open"
+        }
+        data1.push(obj)
+        const data2=JSON.stringify(data1)
+        fs.writeFileSync(filePath,data2)
+        console.log(data2)
+        res.send(JSON.parse(data2))
+    }catch(err){
+        res.send(err)
+    }
+})
+
+// 2 GET /api/items — List the board
+app.get("/api/items",(req,res)=>{
+    try{
+        const data=fs.readFileSync(filePath,"utf-8")
+        console.log(data)
+        res.json(JSON.parse(data))
+    }catch(err){
+        res.send(err)
+    }
+})
+
+//3 GET /api/items/:id — Single item
+app.get("/api/items/:id",(req,res)=>{
+    try{
+        const data=fs.readFileSync(filePath,"utf-8")
+        console.log(data)
+        let data1=JSON.parse(data)
+        let id=req.params.id
+        data1=data1.filter(obj=>obj.id===id)
+        if(data1.length===0){
+            res.status(404).json({ "error": "Not found" })
+        }
+        res.status(200).json(data1)
+    }catch(err){
+        console.log(err);
+        res.send(err)
+    }
+})
+
+//4 PUT /api/items/:id — Edit a report
+// app.put("/api/items/:id",(req,res)=>{
+//     try{
+//         const {id,itemName,type,place,date,contact}=req.body
+//         if(!id || !itemName || !type || !place || !date || !contact){
+//             res.status(400).send("Invalid body")
+//         }
+//     }catch(err){
+//         console.log(err)
+//     }
+// })
+
+app.listen(3000)
